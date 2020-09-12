8cbbc9b7ff15d1a921d7a96fe2c9cb4e230c8546
sitesoft1 <sitesoft1@gmail.com>
2020-09-13 00:13:28 +0300

    statr work

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+set_time_limit(0);//snimaem ogranicheniya na vipolneniya skripta
+/*
+ * https://stackoverflow.com/questions/51627281/woocommerce-rest-api-operation-timed-out
+ *https://searchengines.guru/showthread.php?t=443500
+ */
+//define('IMAGE_PATH', __DIR__ . '/../image/data/xml');
+//define('IMAGE_PATH_TO_DATABASE', 'data/xml');
+
+define('WC_API_DOMAIN', 'https://sushisetboss.com');//PRODUCTION
+define('WC_API_CK', 'ck_5cab96ef0652aa1b744d07a094320fe1bc9b4e5f');
+define('WC_API_CS', 'cs_4ea0e9d3df2cc60a1a92f00aa3f29347aa804fb3');
+define('IMAGE_PATH', __DIR__ . '/../wp-content/uploads/xml');
+define('IMAGE_PATH_TO_DATABASE', 'xml');
+
+define('WC_API_VERSION', 'wc/v3');
+define('WC_API_TIMEOUT', '1800');// curl timeout seconds
+define('LOG_DIR', __DIR__ . '/var');
