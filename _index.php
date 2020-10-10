25865a9bb1bc3210c2196b6913abf05a05f4a94c
sitesoft1 <sitesoft1@gmail.com>
2020-10-10 15:33:11 +0300

    перед правкой заливки отключенных товаров

---
+++ _index.php
@@ -0,0 +1,52 @@
+<?php
+set_time_limit(0);//snimaem ogranicheniya na vipolneniya skripta
+ini_set("memory_limit", "256M");
+
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+//Для бесконечной обработки запросов укажите '0'.
+//ini_set('pm.max_requests', 0);
+
+//My classes
+require_once __DIR__ . '/classes/db.php';
+require_once __DIR__ . '/classes/xml.php';
+require_once __DIR__ . '/functions.php';
+require_once __DIR__ . '/config.php';
+//My classes END
+//Перехват завершения скрипта
+register_shutdown_function('shutdown');
+
+//Composer
+require __DIR__ . '/vendor/autoload.php';
+use Automattic\WooCommerce\Client;
+//Composer END
+
+$db = new Db;
+
+/*
+ * WooCommerce lib
+ * https://packagist.org/packages/automattic/woocommerce
+ * https://woocommerce.github.io/woocommerce-rest-api-docs/
+ * 
+ */
+
+//WooCommerce API CLIENT
+$woocommerce = new Client(
+    WC_API_DOMAIN,
+    WC_API_CK,
+    WC_API_CS,
+    [
+        'version' => WC_API_VERSION,
+        'timeout' => WC_API_TIMEOUT // curl timeout
+    ]
+);
+//WooCommerce API CLIENT END
+
+//print_r($woocommerce);
+
+//dump($woocommerce);
+
+$rez = $db->getProduct('7661', $woocommerce);
+
+dump($rez);
---
+++ import_prices.php
@@ -0,0 +1,81 @@
+<?php
+set_time_limit(0);//snimaem ogranicheniya na vipolneniya skripta
+ini_set("memory_limit", "256M");
+
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+//Для бесконечной обработки запросов укажите '0'.
+//ini_set('pm.max_requests', 0);
+
+//My classes
+require_once __DIR__ . '/classes/db.php';
+require_once __DIR__ . '/classes/xml.php';
+require_once __DIR__ . '/functions.php';
+require_once __DIR__ . '/config.php';
+//My classes END
+//Перехват завершения скрипта
+//register_shutdown_function('shutdown');
+
+//Composer
+require __DIR__ . '/vendor/autoload.php';
+use Automattic\WooCommerce\Client;
+//Composer END
+
+$db = new Db;
+
+/*
+ * WooCommerce lib
+ * https://packagist.org/packages/automattic/woocommerce
+ * https://woocommerce.github.io/woocommerce-rest-api-docs/
+ *
+ */
+
+//WooCommerce API CLIENT
+$woocommerce = new Client(
+    WC_API_DOMAIN,
+    WC_API_CK,
+    WC_API_CS,
+    [
+        'version' => WC_API_VERSION,
+        'timeout' => WC_API_TIMEOUT // curl timeout
+    ]
+);
+//WooCommerce API CLIENT END
+
+//GET opencart data
+if(!empty($_POST)){
+    
+    //$db->log('post_log', $_POST, true);
+    
+    
+	foreach($_POST as $product_id => $value){
+        
+        $name = $value['name'];
+        $price = $value['price'];
+	    
+	 $rez = $db->updateDbPrice($product_id, $name, $price, $woocommerce);
+	 echo $rez;
+		
+		//$db->log('price_import_log', $value, true);
+		//$db->log('price_import_id_log', $product_id, true);
+		
+	}
+ 
+    
+    
+	/*
+    $rezult = $db->addProductFromOpenCart(
+        $wc_product_name,
+        $wc_price,
+        $wc_model,
+        $wc_product_description,
+        $wc_product_images,
+        $woocommerce);
+    echo json_encode($rezult);
+	*/
+}
+//GET opencart data END
+
+//$rezult = $db->getProduct($product_id, $woocommerce);
+
